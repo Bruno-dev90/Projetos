9dc8b472049f4fd9c3d641d446e0d0e9cef01eab
Bruno Antonio <brun0am0reira@hotmail.com>
2021-03-18 22:50:45 -0300

    Rest Testado

--- src/main/java/com/bruno/cursomc/AulajpamavenApplication.java
+++ src/main/java/com/bruno/cursomc/AulajpamavenApplication.java
@@ -1,4 +1,4 @@
-package com.educandoweb.aulajpamaven;
+package com.bruno.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
