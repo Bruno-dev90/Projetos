e48bcbad1c63a1b5cd6b2f3f67b51800c46a440b
Bruno Antonio <brun0am0reira@hotmail.com>
2021-03-23 16:14:16 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/bruno/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/bruno/cursomc/dto/CategoriaDTO.java
@@ -2,9 +2,8 @@ package com.bruno.cursomc.dto;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotEmpty;
-
 import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.NotEmpty;
 
 import com.bruno.cursomc.domain.Categoria;
 
