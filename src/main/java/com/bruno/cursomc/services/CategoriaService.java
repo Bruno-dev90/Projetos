7a94c4dbc0a692d8a65c82d6c5c65400e86a2268
Bruno Antonio <brun0am0reira@hotmail.com>
2021-03-23 09:40:14 -0300

    atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/bruno/cursomc/services/CategoriaService.java
+++ src/main/java/com/bruno/cursomc/services/CategoriaService.java
@@ -19,11 +19,16 @@ public class CategoriaService {
 		 Optional<Categoria> obj = repo.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException( 
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null)); 
-		} 
-  	public Categoria insert(Categoria obj) {
+	} 
+  
+	public Categoria insert(Categoria obj) {
 	  obj.setId(null);
 	  return repo.save(obj);
-	  
-  }
+  	}
+  	
+  	public Categoria update (Categoria obj) {
+  		find(obj.getId());
+  		return repo.save(obj);
+  	}
 
 }
