7398eaa50f9503637b55b9cbb39c1b2595160c4d
Bruno Antonio <brun0am0reira@hotmail.com>
2021-03-25 20:00:09 -0300

    Criando profile de Teste

--- src/main/java/com/bruno/cursomc/domain/pagamentoComCartao.java
+++ src/main/java/com/bruno/cursomc/domain/pagamentoComCartao.java
@@ -3,17 +3,19 @@ package com.bruno.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.bruno.cursomc.enus.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
-public class pagamentoComCartao extends Pagamento {
+@JsonTypeName("pagamentoComCartao")
+public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer numeroDeParcelas;
 	
-	public pagamentoComCartao() {
+	public PagamentoComCartao() {
 		
 	}
-	public pagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
 		super(id, estado, pedido);
 		this.numeroDeParcelas = numeroDeParcelas;
 	}
